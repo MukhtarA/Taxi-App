8edec0473c925f31593cb7184ba33676a5dce547
MukhtarA <33712120+MukhtarA@users.noreply.github.com>
2021-02-09 23:53:18 +0600

    feat: add geolocation

--- App.js
+++ App.js
@@ -6,13 +6,45 @@
  * @flow strict-local
  */
 
-import React from 'react';
-import {StatusBar} from 'react-native';
+import React, {useEffect} from 'react';
+import {PermissionsAndroid, StatusBar, Platform} from 'react-native';
 import HomeScreen from './src/screens/HomeScreen';
 import DestinationSearch from './src/screens/DestinationSearch';
 import SearchResults from './src/screens/SearchResults';
+import Geolocation from '@react-native-community/geolocation';
+navigator.geolocation = require('@react-native-community/geolocation');
 
 const App: () => React$Node = () => {
+  const androidPermission = async () => {
+    try {
+      const granted = await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+        {
+          title: 'App Location Permission',
+          message: 'App needs access to your location ',
+          buttonNeutral: 'Ask Me Later',
+          buttonNegative: 'Cancel',
+          buttonPositive: 'OK',
+        },
+      );
+      if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+        console.log('You can use the location');
+      } else {
+        console.log('Location permission denied');
+      }
+    } catch (err) {
+      console.warn(err);
+    }
+
+    useEffect(() => {
+      if (Platform.OS === 'android') {
+        androidPermission();
+      } else {
+        // request
+        Geolocation.requestAuthorization();
+      }
+    }, []);
+  };
   return (
     <>
       <StatusBar barStyle="dark-content" />
